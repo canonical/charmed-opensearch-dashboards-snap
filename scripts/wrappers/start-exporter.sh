62b7cbce55eab073e6b44a294b227825503e58c4
Gabriel Cocenza <gabriel.cocenza@canonical.com>
2025-01-21 16:30:27 -0300

    - install snapd that is not installed by default on ubuntu-latest - set env vars before running the exporter binary

--- scripts/wrappers/start-exporter.sh
+++ scripts/wrappers/start-exporter.sh
@@ -7,12 +7,14 @@ source "${OPS_ROOT}"/helpers/get-conf.sh
 function fetch_exporter_args () {
     DASHBOARDS_HOST=$( get_yaml_prop "${OPENSEARCH_DASHBOARDS_PATH_CONF}/opensearch_dashboards.yml" "server.host" "localhost" )
     DASHBOARDS_PORT=$( get_yaml_prop "${OPENSEARCH_DASHBOARDS_PATH_CONF}/opensearch_dashboards.yml" "server.port" "5601" )
-    export DASHBOARDS_USERNAME=$( get_yaml_prop "${OPENSEARCH_DASHBOARDS_PATH_CONF}/opensearch_dashboards.yml" "opensearch.username" "kibanaserver" )
-    export OPENSEARCH_DASHBOARDS_PASSWORD=$( get_yaml_prop "${OPENSEARCH_DASHBOARDS_PATH_CONF}/opensearch_dashboards.yml" "opensearch.password" "kibanaserver" )
+    DASHBOARDS_USER=$( get_yaml_prop "${OPENSEARCH_DASHBOARDS_PATH_CONF}/opensearch_dashboards.yml" "opensearch.username" "kibanaserver" )
+    DASHBOARDS_PASSWORD=$( get_yaml_prop "${OPENSEARCH_DASHBOARDS_PATH_CONF}/opensearch_dashboards.yml" "opensearch.password" "kibanaserver" )
     SCHEME=$(snapctl get scheme)
 }
 
 function start_exporter () {
+    OPENSEARCH_DASHBOARDS_USER="${DASHBOARDS_USER}" \
+    OPENSEARCH_DASHBOARDS_PASSWORD="${DASHBOARDS_PASSWORD}" \
     exec "${SNAP}"/usr/bin/setpriv \
         --clear-groups \
         --reuid snap_daemon \
